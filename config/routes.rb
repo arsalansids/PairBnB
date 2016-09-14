9fb56c67e3be9f3c33253adfbf403edaab15501e
Arsalan Siddiqui <Arsalan@MacBook-Air-2.local>
2016-09-14 18:25:06 +0800

    added clearance

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -54,3 +54,6 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
+
+#rake routes, this will show all of the current routes in terminal. 
+#You can add them in this routes page
